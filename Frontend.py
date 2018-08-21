223d8a33231ad3f6887cad9df0091a7884162416
William Van <wvanniew@gmail.com>
2018-08-21 14:25:26 +0200

    Corrected a small bug

--- Frontend.py
+++ Frontend.py
@@ -22,16 +22,19 @@ def add_command():
 
 def get_selected_row(event):
     global selected_tuple
-    index=list1.curselection()[0]
-    selected_tuple=list1.get(index)
-    e1.delete(0,END)
-    e1.insert(END,selected_tuple[1])
-    e2.delete(0,END)
-    e2.insert(END,selected_tuple[2])
-    e3.delete(0,END)
-    e3.insert(END,selected_tuple[3])
-    e4.delete(0,END)
-    e4.insert(END,selected_tuple[4])
+    if list1.curselection():
+        index=list1.curselection()[0]
+        selected_tuple=list1.get(index)
+        e1.delete(0,END)
+        e1.insert(END,selected_tuple[1])
+        e2.delete(0,END)
+        e2.insert(END,selected_tuple[2])
+        e3.delete(0,END)
+        e3.insert(END,selected_tuple[3])
+        e4.delete(0,END)
+        e4.insert(END,selected_tuple[4])
+    else:
+        selected_tuple=None
 
 
 def delete_command():
