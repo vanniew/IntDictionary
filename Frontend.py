54e2a26ddf5cde27b7734247e6f55b00d4c94200
William Van <wvanniew@gmail.com>
2018-08-21 13:08:51 +0200

    Added the backend function

--- Frontend.py
+++ Frontend.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import backend
 
 window = Tk()
 
@@ -18,19 +19,46 @@ l4.grid(row=1, column=2)
 
 title_text=StringVar()
 e1=Entry(window, textvariable=title_text)
-e1.grid(row=0,column=1)
+e1.grid(row=0, column=1)
 
 author_text=StringVar()
 e1=Entry(window, textvariable=author_text)
-e1.grid(row=0,column=3)
+e1.grid(row=0, column=3)
 
 year_text=StringVar()
 e1=Entry(window, textvariable=year_text)
-e1.grid(row=1,column=1)
+e1.grid(row=1, column=1)
 
 ISBN_text=StringVar()
 e1=Entry(window, textvariable=ISBN_text)
-e1.grid(row=1,column=3)
+e1.grid(row=1, column=3)
+
+list1=Listbox(window, height=6, width=35)
+list1.grid(row=2, column=0, rowspan=6, columnspan=2)
+
+sb1=Scrollbar(window, orient=VERTICAL)
+sb1.grid(row=2, column=2, rowspan=6)
+
+b1=Button(window, text='View all', width=12, command=backend.view)
+b1.grid(row=2,column=3)
+
+b2=Button(window, text='Search Entry', width=12)
+b2.grid(row=3,column=3)
+
+b3=Button(window, text='Add Entry', width=12)
+b3.grid(row=4,column=3)
+
+b4=Button(window, text='Update', width=12)
+b4.grid(row=5,column=3)
+
+b5=Button(window, text='Delete', width=12)
+b5.grid(row=6,column=3)
+
+b6=Button(window, text='Close', width=12)
+b6.grid(row=7,column=3)
+
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
 
 
 window.mainloop()
